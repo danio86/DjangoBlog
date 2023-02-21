56e8760d94b96577bfafd8cc4110e0964fb8f244
danio86 <d.harder@gmx.at>
2023-02-21 16:44:36 +0000

    add env.py and .ignore

---
+++ .env.py
@@ -0,0 +1,4 @@
+import os
+    
+os.environ["DATABASE_URL"] = "postgres://zsxstzqw:dm1n_CAMikXmWBfderPfSQnTePQLtva1@manny.db.elephantsql.com/zsxstzqw"
+os.environ["SECRET_KEY"] = "1wqzz@pk1fo*5w-nv_dfbfo1)82m)(m(_q)akr7ae=*t0l)rs-"
