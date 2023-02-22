23f2ea698dd0f2b5a835ec7b3815670820617ec2
danio86 <d.harder@gmx.at>
2023-02-22 13:04:07 +0000

    removed typo in env.py

--- .env.py
+++ .env.py
@@ -2,4 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://zsxstzqw:dm1n_CAMikXmWBfderPfSQnTePQLtva1@manny.db.elephantsql.com/zsxstzqw"
 os.environ["SECRET_KEY"] = "radomAnyKey1986"
-os.environ["COUDINARY_URL"] = "cloudinary://327836945551748:YMPtuzBnuEYN5ijgQulYKZbeZdk@ddigiimwd"
+os.environ["CLOUDINARY_URL"] = "cloudinary://327836945551748:YMPtuzBnuEYN5ijgQulYKZbeZdk@ddigiimwd"
