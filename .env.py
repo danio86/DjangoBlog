5c5dfc25ab75fe8f750deca146ce007ac8848ebf
danio86 <d.harder@gmx.at>
2023-02-22 13:31:10 +0000

    changed env.py typo

--- .env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgres://zsxstzqw:dm1n_CAMikXmWBfderPfSQnTePQLtva1@manny.db.elephantsql.com/zsxstzqw"
-os.environ["SECRET_KEY"] = "radomAnyKey1986"
-os.environ["CLOUDINARY_URL"] = "cloudinary://327836945551748:YMPtuzBnuEYN5ijgQulYKZbeZdk@ddigiimwd"
--- codestar/settings.py
+++ codestar/settings.py
@@ -90,10 +90,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #     }
 # }
 
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
